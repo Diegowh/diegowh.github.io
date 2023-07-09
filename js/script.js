2062ad71487a5d38b28973aacf435dc2ca0e4517
Diegowh <contacto.diegowh@gmail.com>
2023-07-09 12:30:55 +0200

    Add some comments to script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// Selecciona el header del DOM
 const header = document.querySelector("header");
 
+// Añade la clase 'sticky' al header al desplazarnos
 window.addEventListener ("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY >0);
 });
@@ -7,16 +9,20 @@ window.addEventListener ("scroll", function() {
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+
+// Gestiona la apertura y cierre del menú al hacer click
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
 
+// Cierra el menu al hacer scroll
 window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
 
+// Configura ScrollReveal para las animaciones al hacer scroll
 const sr = ScrollReveal (
     {
         distance: '25px',
@@ -25,5 +31,6 @@ const sr = ScrollReveal (
     }
 )
 
+// Aplica el ScrollReveal en diferentes secciones de la pagina
 sr.reveal('.home-text', {delay: 190, origin: 'bottom'})
 sr.reveal('.about, .services, .portfolio, .contact', {delay: 200, origin: 'bottom'})
